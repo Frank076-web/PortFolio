5442d68ebc6c38322e5663854a066b4b422ec74c
Frank076-web <83378749+Frank076-web@users.noreply.github.com>
2022-03-06 17:10:54 -0300

    Development (#3)
    * added and configure sass
    
    * added some sass files for config and changed header
    
    * added social media and reeplace bootstrap for main to sass
    
    * fixing whatsapp number
    
    * change href whatsapp
    
    * upgrade mixins media queries
    
    * change bootstrap for sass in skills and experience sections
    
    * fixing color h2 redes sociales

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,9 +1,7 @@
-import Link from 'next/link';
-
 const Header = () => {
 	return (
 		<header className="header">
-			<p>PorFolio</p>
+			<p>PortFolio</p>
 		</header>
 	);
 };
--- components/index/SectionExperience.jsx
+++ components/index/SectionExperience.jsx
@@ -2,71 +2,57 @@ import { education, experience } from '../../profile';
 
 const SectionExperience = () => {
 	return (
-		<section className="col-md-8">
-			<div className="card bg-light">
-				<div className="card-body">
-					<h2 className="text-primary">Experiencia Profesional</h2>
-					<ul>
-						{experience.map(
-							({ title, company, duration, description }, index) => (
-								<li key={index}>
-									<h4>
-										<bold>{title}</bold>
-									</h4>
-									<h6>
-										{company} | {duration}
-									</h6>
-									<p>{description}</p>
-								</li>
-							)
-						)}
-					</ul>
+		<section className="section-experience">
+			<h2>Experiencia Profesional</h2>
+			<ul>
+				{experience.map(
+					({ title, company, duration, description }, index) => (
+						<li key={index}>
+							<h4>
+								<bold>{title}</bold>
+							</h4>
+							<h6>
+								{company} | {duration}
+							</h6>
+							<p>{description}</p>
+						</li>
+					)
+				)}
+			</ul>
 
-					<h2 className="text-primary">Historial Académico</h2>
-					<ul>
-						{education.map(
-							(
-								{ institution, career, duration, description },
-								index
-							) => (
-								<li key={index}>
-									<h4>
-										<bold>{institution}</bold>
-									</h4>
-									<h6>
-										{career ? career : null}{' '}
-										{duration ? duration : null}
-									</h6>
-									<ul>
-										{Object.keys(description).map(
-											(element, index) => (
-												<li
-													style={{
-														listStyle: 'none'
-													}}
-													key={index}
-												>
-													<p>{description[element]}</p>
-												</li>
-											)
-										)}
-									</ul>
-								</li>
-							)
-						)}
+			<h2>Historial Académico</h2>
+			<ul>
+				{education.map(
+					({ institution, career, duration, description }, index) => (
+						<li key={index}>
+							<h4>
+								<bold>{institution}</bold>
+							</h4>
+							<h6>
+								{career ? career : null} {duration ? duration : null}
+							</h6>
+							<ul>
+								{Object.keys(description).map((element, index) => (
+									<li
+										style={{
+											listStyle: 'none'
+										}}
+										key={index}
+									>
+										<p>{description[element]}</p>
+									</li>
+								))}
+							</ul>
+						</li>
+					)
+				)}
 
-						<h2 className="text-primary">Intereses</h2>
-						<ul>
-							<li style={{ listStyle: 'none' }}>
-								<p>Desarrollo Web FrontEnd</p>
-							</li>
-							<li style={{ listStyle: 'none' }}>
-								<p>Desarrollo Web BackEnd</p>
-							</li>
-						</ul>
-					</ul>
+				<h2>Intereses</h2>
+				<div className="section-experience__interest">
+					<p>Desarrollo Web FrontEnd</p>
+					<p>Desarrollo Web BackEnd</p>
 				</div>
-			</div>
+			</ul>
 		</section>
 	);
 };
