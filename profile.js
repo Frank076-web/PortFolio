23d5f96c6cda3822d71141c3d9a1e1d0a5897b65
Franco Elias Inzerillo <francoeinzerillo@gmail.com>
2022-05-15 19:40:52 -0300

    Added classes for profile and a new skill and education for Java

--- profile.js
+++ profile.js
@@ -1,113 +1,38 @@
+import { Skill, Experience, Education, Project } from "./utils/classesProfile";
+
 export const skills = [
-	{
-		name: 'Html',
-		image: '/images/HTML5.png',
-		alt: 'html',
-		level: '90%'
-	},
-	{
-		name: 'Css',
-		image: '/images/CSS3.png',
-		alt: 'css',
-		level: '85%'
-	},
-	{
-		name: 'Sass',
-		image: '/images/Sass.png',
-		alt: 'sass',
-		level: '80%'
-	},
-	{
-		name: 'Javascript',
-		image: '/images/Javascript.png',
-		alt: 'javascript',
-		level: '90%'
-	},
-	{
-		name: 'React',
-		image: '/images/REACT.png',
-		alt: 'react',
-		level: '90%'
-	},
-	{
-		name: 'NextJS',
-		image: '/images/NextJs.png',
-		alt: 'nextjs',
-		level: '30%'
-	}
+    new Skill("Html", "HTML5.png", "html", "90%"),
+    new Skill("Css", "CSS3.png", "css", "85%"),
+    new Skill("Javascript", "Javascript.png", "javascript", "90%"),
+    new Skill("React", "REACT.png", "react", "90%"),
+    new Skill("Sass", "Sass.png", "sass", "80%"),
+    new Skill("Java", "java.png", "java", "40%"),
+    new Skill("NextJS", "NextJs.png", "nextjs", "30%"),
+    new Skill("Git", "git.png", "git", "50%"),
 ];
 
 export const experience = [
-	{
-		title: 'Frontend Developer React',
-		company: 'Alkemy',
-		duration: 'Enero-2022 Hasta el presente',
-		description: {
-			first: 'Simulación de trabajo real en equipo.',
-			second: 'Manejo de repositorios Git con Github.',
-			third: 'Metodologías Ágiles (Scrum).',
-			fourth: 'Uso de la herramienta Jira'
-		}
-	},
-	{
-		title: 'Tecnico Electrónico',
-		company: 'Laboratorios Eléctricos Argentinos S.A',
-		duration: '2016-Hasta el presente',
-		description:
-			'	Reparación, verificación y asistencia técnica de	equipos cable módem, decodificadores de CATV (analógicos y digitales). Experiencia en trabajo individual, en equipo y línea de producción.	Reparaciones a nivel soldadura superficial SMD'
-	},
-	{
-		title: 'Tecnico Electrónico',
-		company: 'STILUX. Electrónica industrial',
-		duration: 'Mayo-2015 Julio-2015',
-		description: 'Montaje y soldado de componentes electrónicos. Empleado en pasantías por 200 horas.'
-	}
+    //Experience => title, company, duration, description
+    new Experience("Frontend Developer React", "Alkemy", "Enero-2022 Hasta el presente", ["Simulación de trabajo real en equipo.", "Manejo de repositorios Git con Github.", "Metodologías Ágiles (Scrum).", "Uso de la herramienta Jira"]),
+    new Experience(
+        "Técnico Electrónico",
+        "Laboratorios Eléctricos Argentinos S.A",
+        "2016-Hasta el presente",
+        "Reparación, verificación y asistencia técnica de	equipos cable módem, decodificadores de CATV (analógicos y digitales). Experiencia en trabajo individual, en equipo y línea de producción.	Reparaciones a nivel soldadura superficial SMD",
+    ),
+    new Experience("Tecnico Electrónico", "STILUX. Electrónica industrial", "Mayo-2015 Julio-2015", "Montaje y soldado de componentes electrónicos. Empleado en pasantías por 200 horas."),
 ];
 
 export const education = [
-	{
-		institution: 'UDEMY',
-		description: {
-			first: 'Desarrollo web HTML, CSS, Javascript.',
-			second: 'Javascript Moderno.',
-			third: 'React.'
-		}
-	},
-	{
-		institution: 'Autodidacta',
-		description: {
-			first: 'Plataformas como youtube.',
-			second: 'Leyendo documentación oficial.'
-		}
-	},
-	{
-		institution: 'Instituto Parroquial San Juan XXIII',
-		career: 'Técnico Electrónico',
-		duration: '2010-2015',
-		description: {
-			first: 'Análisis, Medición y calculos de circuitos electrónicos.',
-			second: 'Programación en C y microcontroladores.'
-		}
-	}
+    //Education => institution, career, duration, description
+    new Education("UDEMY", "", "", ["Desarrollo web HTML, CSS, Javascript.", "Javascript Moderno.", "React.", "Máster completo en Java."]),
+    new Education("Autodidacta", "", "", ["Plataformas como youtube.", "Leyendo documentación oficial."]),
+    new Education("Instituto Parroquial San Juan XXIII", "Técnico Electrónico", "2010-2015", ["Análisis, Medición y calculos de circuitos electrónicos.", "Programación en C y microcontroladores."]),
 ];
 
 export const projects = [
-	{
-		image: '/images/portada somos mas.png',
-		title: 'Somos Más',
-		description: 'Página web para la ong Somos Más, centro de ayuda comunitaria',
-		link: 'https://ongsomosmasfrank076.netlify.app'
-	},
-	{
-		image: '/images/marvel.jpg',
-		title: 'Buscador Héroes Marvel',
-		description: 'Página web buscador de heroes , armá tu equipo.',
-		link: 'https://buscadorheroesalkemy.netlify.app'
-	},
-	{
-		image: '/images/where-in-the-world.jpg',
-		title: 'Buscador Países',
-		description: 'Página web para buscar países de todo el mundo',
-		link: 'https://where-in-the-world-frank076web.netlify.app/'
-	}
+    //Projects => image, title, description, link
+    new Project("/images/portada somos mas.png", "Somos Más", "Página web para la ong Somos Más, centro de ayuda comunitaria", "https://ongsomosmasfrank076.netlify.app"),
+    new Project("/images/marvel.jpg", "Buscador Héroes Marvel", "Página web buscador de heroes , armá tu equipo.", "https://buscadorheroesalkemy.netlify.app"),
+    new Project("/images/where-in-the-world.jpg", "Buscador Países", "Página web para buscar países de todo el mundo", "https://where-in-the-world-frank076web.netlify.app/"),
 ];
