e6c070978516d3dfbe0d10d8e5abceaf1717ebb5
Franco Inzerillo <83378749+Frank076-web@users.noreply.github.com>
2022-05-15 19:48:43 -0300

    Development (#11)
    * finished v 2.0
    
    * remove useEffect unused
    
    * trying to fix deploy problem with gradient progressBar
    
    * timing animation
    
    * timing animation
    
    * changes ubications and link
    
    * fixed experience alkemy and changed phone number
    
    * added project and change experience alkemy
    
    * Change Image and modifying some styles
    
    * Fixed mail to, missing l
    
    * Merge branch 'main' of https://github.com/Frank076-web/PortFolio into development
    
    * Added classes for profile and a new skill and education for Java
    
    * Deleted console.log
    
    Co-authored-by: Franco Elias Inzerillo <francoeinzerillo@gmail.com>

--- components/index/SectionExperience.jsx
+++ components/index/SectionExperience.jsx
@@ -1,91 +1,76 @@
-import { education, experience } from '../../profile';
+import { education, experience } from "../../profile";
 
 const SectionExperience = () => {
-	return (
-		<section className="section-experience">
-			<div className="container">
-				<h2>Experiencia Profesional</h2>
-				<ul>
-					{experience.map(
-						({ title, company, duration, description }, index) => (
-							<li
-								key={index}
-								className="section-experience__experience"
-							>
-								<h4>
-									<bold>{title}</bold>
-								</h4>
-								<div className="container">
-									<h6>
-										{company} | {duration}
-									</h6>
-									<ul>
-										{typeof description === 'object' ? (
-											Object.keys(description).map(
-												(element, index) => (
-													<li
-														style={{
-															listStyle: 'circle'
-														}}
-														key={index}
-													>
-														<p>{description[element]}</p>
-													</li>
-												)
-											)
-										) : (
-											<p>{description}</p>
-										)}
-									</ul>
-								</div>
-							</li>
-						)
-					)}
-				</ul>
+    return (
+        <section className="section-experience">
+            <div className="container">
+                <h2>Experiencia Profesional</h2>
+                <ul>
+                    {experience.map(({ title, company, duration, description }, index) => (
+                        <li key={index} className="section-experience__experience">
+                            <h4>
+                                <bold>{title}</bold>
+                            </h4>
+                            <div className="container">
+                                <h6>
+                                    {company} | {duration}
+                                </h6>
+                                <ul>
+                                    {typeof description === "object" ? (
+                                        Object.keys(description).map((element, index) => (
+                                            <li
+                                                style={{
+                                                    listStyle: "circle",
+                                                }}
+                                                key={index}
+                                            >
+                                                <p>{description[element]}</p>
+                                            </li>
+                                        ))
+                                    ) : (
+                                        <p>{description}</p>
+                                    )}
+                                </ul>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
 
-				<h2>Historial Académico</h2>
-				<ul>
-					{education.map(
-						(
-							{ institution, career, duration, description },
-							index
-						) => (
-							<li className="section-experience__studies" key={index}>
-								<h4>{institution}</h4>
-								<div className="container">
-									<h6>
-										{career ? career : null}{' '}
-										{duration ? duration : null}
-									</h6>
+                <h2>Historial Académico</h2>
+                <ul>
+                    {education.map(({ institution, career, duration, description }, index) => (
+                        <li className="section-experience__studies" key={index}>
+                            <h4>{institution}</h4>
+                            <div className="container">
+                                <h6>
+                                    {career ? career : null} {duration ? duration : null}
+                                </h6>
 
-									<ul>
-										{Object.keys(description).map(
-											(element, index) => (
-												<li
-													style={{
-														listStyle: 'none'
-													}}
-													key={index}
-												>
-													<p>{description[element]}</p>
-												</li>
-											)
-										)}
-									</ul>
-								</div>
-							</li>
-						)
-					)}
-				</ul>
+                                <ul>
+                                    {Object.keys(description).map((element, index) => (
+                                        <li
+                                            style={{
+                                                listStyle: "none",
+                                            }}
+                                            key={index}
+                                        >
+                                            <p>{description[element]}</p>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
 
-				<div className="section-experience__interest">
-					<h2>Intereses</h2>
-					<p>Desarrollo Web FrontEnd</p>
-					<p>Desarrollo Web BackEnd</p>
-				</div>
-			</div>
-		</section>
-	);
+                <div className="section-experience__interest">
+                    <h2>Intereses</h2>
+                    <p>Desarrollo Web FrontEnd</p>
+                    <p>Desarrollo Web BackEnd</p>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default SectionExperience;
