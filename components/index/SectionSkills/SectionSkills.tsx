d772c174475f0131d25cd584108006695b9aead7
Franco Elias Inzerillo <francoeinzerillo@gmail.com>
2023-03-06 19:41:22 -0300

    Added Experience and Study

--- components/index/SectionSkills/SectionSkills.tsx
+++ components/index/SectionSkills/SectionSkills.tsx
@@ -1,15 +1,15 @@
-import { skills } from "../../../utils/profile";
+import { skills } from "../../../utils/profile.config";
 import Skills from "./Skills";
 
 const SectionSkills = (): JSX.Element => {
-    return (
-        <section className="section-skills">
-            <div className="container">
-                <h2>Skills</h2>
-                <Skills skills={skills} />
-            </div>
-        </section>
-    );
+      return (
+            <section className="section-skills">
+                  <div className="container">
+                        <h2>Skills</h2>
+                        <Skills skills={skills} />
+                  </div>
+            </section>
+      );
 };
 
 export default SectionSkills;
